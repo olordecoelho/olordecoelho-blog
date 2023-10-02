e128cc06cdcb5b7340bf7f459dba995bd1edf364
olordecoelho <olordecoelho@gmail.com>
2023-10-02 13:22:55 -0300

    Inicial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace olordecoelho_blog.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
